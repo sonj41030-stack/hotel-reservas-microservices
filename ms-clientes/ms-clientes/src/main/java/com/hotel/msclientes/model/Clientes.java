a89d678f0cefdaabfe3809ab895507cb41f2fdbd
Edixon Cabriless <sonj41030@gmail.com>
2026-05-09 19:22:12 -0400

    feat: ms-clientes completo con Flyway y migracion SQL

--- ms-clientes/ms-clientes/src/main/java/com/hotel/msclientes/model/Clientes.java
+++ ms-clientes/ms-clientes/src/main/java/com/hotel/msclientes/model/Clientes.java
@@ -3,7 +3,6 @@ package com.hotel.msclientes.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
