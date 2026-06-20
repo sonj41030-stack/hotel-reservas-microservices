00b007793931626af78e28a6f86160445e45d14c
Cross Wolf <springtrapgamer3@gmail.com>
2026-06-19 23:37:52 -0400

    lo mismo, agregado de los Test en reportes

--- ms-reportes/ms-reportes/src/test/java/com/hotel/msreportes/MsReportesApplicationTests.java
+++ ms-reportes/ms-reportes/src/test/java/com/hotel/msreportes/MsReportesApplicationTests.java
@@ -3,11 +3,14 @@ package com.hotel.msreportes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.flyway.enabled=false",
+        "eureka.client.enabled=false"
+})
 class MsReportesApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
