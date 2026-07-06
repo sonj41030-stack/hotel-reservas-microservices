3b66be6bbdd6f3fa510073c410ab4e35395c504c
Cross Wolf <springtrapgamer3@gmail.com>
2026-07-06 19:44:08 -0400

    fix: activa perfil docker en Dockerfiles y corrige datasource de mis 3 microservicios

--- ms-housekeeping/target/classes/db/migration/V2__add_missing_columns_housekeeping.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE tareas_housekeeping
-    ADD COLUMN tipo_tarea VARCHAR(50) NOT NULL DEFAULT 'LIMPIEZA',
-    ADD COLUMN empleado_asignado VARCHAR(100),
-    ADD COLUMN prioridad VARCHAR(20) DEFAULT 'MEDIA',
-    ADD COLUMN observaciones TEXT;
