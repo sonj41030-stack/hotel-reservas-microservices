86fa7e206e42cb71c84e210e12fde2d11be289a0
Cross Wolf <73313569+carlkestys@users.noreply.github.com>
2026-07-12 21:10:18 -0400

    Delete ms-servicios directory

--- ms-servicios/src/main/java/com/hotel/msservicios/exception/servicioNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hotel.msservicios.exception;
-
-public class ServicioNotFoundException extends RuntimeException {
-    public ServicioNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
